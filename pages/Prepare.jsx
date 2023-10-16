41e85b13bd0ea7f8264e1dfe8a376b7e5cbed485
cdehaan <cdehaan@gmail.com>
2023-10-16 20:48:12 +0900

    Added image and date modal for Prepare component

--- pages/Prepare.jsx
+++ pages/Prepare.jsx
@@ -3,16 +3,28 @@ import styles from "./index.module.css";
 
 function Prepare({ data, setData }) {
     const [menuOpen, setMenuOpen] = useState(false);
+    const [imageModalOpen, setImageModalOpen] = useState(false);
+    const [dateModalOpen, setDateModalOpen] = useState(false);
+    const [selectedDate, setSelectedDate] = useState(data.healthExaminationDate || "");
+    
+    const menuModalRef = useRef(null);
+    const imageModalRef = useRef(null);
+    const examinationModalRef = useRef(null);
 
-    const logoutButton = <button onClick={data.logout}>Logout</button>
-    const deleteDataButton = <button onClick={data.resetData}>Delete</button>
-
-    const modalRef = useRef(null);
+    function handleDateChange(event) {
+        setSelectedDate(event.target.value);
+    }
 
     useEffect(() => {
         function handleClickOutside(event) {
-            if (modalRef.current && !modalRef.current.contains(event.target)) {
+            if (menuModalRef.current && !menuModalRef.current.contains(event.target)) {
                 setMenuOpen(false);
+            }
+            if (imageModalRef.current && !imageModalRef.current.contains(event.target)) {
+                setImageModalOpen(false);
+            }
+            if (examinationModalRef.current && !examinationModalRef.current.contains(event.target)) {
+                setDateModalOpen(false);
             }
         }
 
@@ -25,30 +37,84 @@ function Prepare({ data, setData }) {
     function DummyFunction() {
         console.log("Task action clicked!");
     }
+
+    function UploadImage() {
+        setImageModalOpen(!imageModalOpen)
+    }
+
     const bearSpeech = (
         <span>
-          Hello {data.username}, we're getting ready for your leave of absence.
-          <br />
+          Hello {data.username}, we're getting ready for your leave of absence.<br />
           Here's what we need to do.
         </span>
-      );
-          
+    );
+
+    const menuModal = (
+        <div className={`${styles.modalBackground} ${styles.logoutModal}`}>
+            <div className={styles.modal} ref={menuModalRef}>
+                <button onClick={data.logout}>Logout</button>
+                <button onClick={data.resetData}>Delete</button>
+            </div>
+        </div>
+    )
+
+    const imageModal = (
+        <div className={styles.modalBackground}>
+            <div className={styles.modal} ref={imageModalRef}>
+                {data.medicalCertificateSubmitted ? <>
+                    <img src="/medicalCertificate.jpg" alt="Uploaded Image Preview" className={styles.uploadedImage} />
+                    Image uploaded!</>
+                    : <>
+                    <img src="/document.svg" alt="Uploaded Image Preview" className={styles.uploadedImage} />
+                    No image</>}
+                <div className={styles.modalButtonContainer}>
+                    <button onClick={() => {alert("Upload image dialog here"); setData(prevData => ({ ...prevData, medicalCertificateSubmitted: true}))}}>Upload</button>
+                    <button onClick={() => {if(confirm("Delete image?")) setData(prevData => ({ ...prevData, medicalCertificateSubmitted: false}))}}>Delete</button>
+                </div>
+            </div>
+        </div>
+    )
+
+    const dateModal = (
+        <div className={styles.modalBackground}>
+            <div className={styles.modal} ref={examinationModalRef}>
+                <input 
+                    type="date" 
+                    value={selectedDate} 
+                    onChange={handleDateChange}
+                    className={styles.dateInput}
+                />
+                <div className={styles.modalButtonContainer}>
+                    <button onClick={() => {
+                        if(selectedDate === '') {
+                            alert("Please pick a date.");
+                            return
+                        }
+                        alert("Date set!");
+                        console.log(selectedDate);
+                        setDateModalOpen(false);
+                        setData(prevData => ({ ...prevData, healthExaminationSubmitted:true, healthExaminationDate: selectedDate }))
+                    }}>Register</button>
+                    <button onClick={() => {
+                        alert("Date removed");
+                        setDateModalOpen(false);
+                        setSelectedDate('');
+                        setData(prevData => ({ ...prevData, healthExaminationSubmitted:false, healthExaminationDate: ''}))
+                    }}>Delete</button>
+                </div>
+            </div>
+        </div>
+    )
+
     return (
         <>
             <div className={styles.menuContainer}>
-                <div className={styles.hamburgerIcon} onClick={() => setMenuOpen(!menuOpen)}>
-                    ☰
-                </div>
-                
-                {menuOpen && (
-                    <div className={styles.modalBackground}>
-                        <div className={styles.modal} ref={modalRef}>
-                            <button onClick={data.logout}>Logout</button>
-                            <button onClick={data.resetData}>Delete</button>
-                        </div>
-                    </div>
-                )}
+                <div className={styles.hamburgerIcon} onClick={() => setMenuOpen(!menuOpen)}>☰</div>
+                {menuOpen && menuModal}
             </div>
+            {imageModalOpen && imageModal}
+            {dateModalOpen && dateModal}
+
             <div className={styles.mascotContainer}>
                 <div className={styles.speechBubble}>{bearSpeech}</div>
                 <img src="/bear.png" alt="Bear Mascot" className={styles.mascotImage} />
@@ -57,41 +123,39 @@ function Prepare({ data, setData }) {
             <div className={styles.taskListContainer}>
                 <div className={styles.taskItem}>
                     <div className={data.applicationForLeaveOfAbsenceSubmitted ? styles.taskDone : styles.taskPending}>
-                        Submit Application for Leave of Absence
+                        {data.applicationForLeaveOfAbsenceSubmitted ? "☑" : "☐"} Submit Application for Leave of Absence
                     </div>
                     <button className={styles.taskActionButton} onClick={DummyFunction}>Complete Task</button>
                 </div>
 
                 <div className={styles.taskItem}>
                     <div className={data.medicalCertificateSubmitted ? styles.taskDone : styles.taskPending}>
-                        Submit a Medical Certificate
+                        {data.medicalCertificateSubmitted ? "☑" : "☐"} Submit a Medical Certificate
                     </div>
-                    <button className={styles.taskActionButton} onClick={DummyFunction}>Upload Image</button>
+                    <button className={styles.taskActionButton} onClick={UploadImage}>Upload Image</button>
                 </div>
 
                 <div className={styles.taskItem}>
                     <div className={data.attendanceStatusSubmitted ? styles.taskDone : styles.taskPending}>
-                        Verify Attendance Status
+                        {data.attendanceStatusSubmitted ? "☑" : "☐"} Verify Attendance Status
                     </div>
                     <button className={styles.taskActionButton} onClick={DummyFunction}>Verify</button>
                 </div>
 
                 <div className={styles.taskItem}>
                     <div className={data.healthExaminationSubmitted ? styles.taskDone : styles.taskPending}>
-                        Complete a Health Examination
+                        {data.healthExaminationSubmitted ? "☑" : "☐"} Complete a Health Examination
                     </div>
-                    <button className={styles.taskActionButton} onClick={DummyFunction}>Schedule Exam</button>
+                    <button className={styles.taskActionButton} onClick={() => setDateModalOpen(!dateModalOpen)}>Schedule Exam</button>
                 </div>
 
                 <div className={styles.taskItem}>
                     <div className={data.systemUseConsent ? styles.taskDone : styles.taskPending}>
-                        Give Consent for daily activity collection
+                        {data.systemUseConsent ? "☑" : "☐"} Give Consent for activity tracking
                     </div>
                     <button className={styles.taskActionButton} onClick={() => setData(prevData => ({ ...prevData, systemUseConsent: !prevData.systemUseConsent}))}>{data.systemUseConsent ? "Remove Consent" : "Give Consent"}</button>
                 </div>
             </div>
-            {logoutButton}
-            {deleteDataButton}<br/>
       </>
     )
 }
